b0c331e0d297d65da545bd1b5ae68848f339e3a3
Sam Coianiz <coianizs@gmail.com>
2020-08-24 08:37:28 +1000

    Still following the tutorial but it's not really looking good. There are a lot of errors and the site doesn't build at the moment.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { HomeComponent } from "./home/home.component";
 import {DetailsComponent} from './details/details.component';
 import {AddComponent} from './add/add.component';
 import {LoginComponent} from './login/login.component';
-//import {SignupComponent} from './signup/signup.component';
+import {SignupComponent} from './signup/signup.component';
+import {AuthGuard} from './auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'details/:city', component: DetailsComponent},
   {path:'add', component: AddComponent},
   {path: 'login', component: LoginComponent, canActivate: [AuthGuard]},
-  //{path: 'signup', component: SignupComponent, canActivate: [AuthGuard]},
+  {path: 'signup', component: SignupComponent, canActivate: [AuthGuard]},
   {path: '**', redirectTo: ''}
 ];
 
